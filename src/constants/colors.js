f3a98c81a50124d79f93156f458070a5ac7d0abe
Aryaman Rishabh <aryamanrishabh@gmail.com>
2023-10-03 00:58:34 -0400

    add constants

---
+++ src/constants/colors.js
@@ -0,0 +1,68 @@
+export const WHITE = "#fff";
+export const BLACK = "#000";
+
+// TODO: REMOVE
+export const GREY = "#7A7E86";
+export const AZURE = "#1294f2";
+export const MIDNIGHT = "#001f31";
+export const WHITE_SMOKE = "#f3f4f6";
+
+export const GRAY_25 = "#FCFCFD";
+export const GRAY_50 = "#F9FAFB";
+export const GRAY_100 = "#F2F4F7";
+export const GRAY_200 = "#EAECF0";
+export const GRAY_300 = "#D0D5DD";
+export const GRAY_400 = "#98A2B3";
+export const GRAY_500 = "#667085";
+export const GRAY_600 = "#475467";
+export const GRAY_700 = "#344054";
+export const GRAY_800 = "#1D2939";
+export const GRAY_900 = "#101828";
+
+export const PRIMARY_25 = "#F5FAFF";
+export const PRIMARY_50 = "#EFF8FF";
+export const PRIMARY_100 = "#D1E9FF";
+export const PRIMARY_200 = "#B2DDFF";
+export const PRIMARY_300 = "#84CAFF";
+export const PRIMARY_400 = "#53B1FD";
+export const PRIMARY_500 = "#2E90FA";
+export const PRIMARY_600 = "#1570EF";
+export const PRIMARY_700 = "#175CD3";
+export const PRIMARY_800 = "#1849A9";
+export const PRIMARY_900 = "#194185";
+
+export const ERROR_25 = "#FFFBFA";
+export const ERROR_50 = "#FEF3F2";
+export const ERROR_100 = "#FEE4E2";
+export const ERROR_200 = "#FECDCA";
+export const ERROR_300 = "#FDA29B";
+export const ERROR_400 = "#F97066";
+export const ERROR_500 = "#F04438";
+export const ERROR_600 = "#D92D20";
+export const ERROR_700 = "#B42318";
+export const ERROR_800 = "#912018";
+export const ERROR_900 = "#7A271A";
+
+export const WARNING_25 = "#FFFCF5";
+export const WARNING_50 = "#FFFAEB";
+export const WARNING_100 = "#FEF0C7";
+export const WARNING_200 = "#FEDF89";
+export const WARNING_300 = "#FEC84B";
+export const WARNING_400 = "#FDB022";
+export const WARNING_500 = "#F79009";
+export const WARNING_600 = "#DC6803";
+export const WARNING_700 = "#B54708";
+export const WARNING_800 = "#93370D";
+export const WARNING_900 = "#7A2E0E";
+
+export const SUCCESS_25 = "#F6FEF9";
+export const SUCCESS_50 = "#ECFDF3";
+export const SUCCESS_100 = "#D1FADF";
+export const SUCCESS_200 = "#A6F4C5";
+export const SUCCESS_300 = "#6CE9A6";
+export const SUCCESS_400 = "#32D583";
+export const SUCCESS_500 = "#12B76A";
+export const SUCCESS_600 = "#039855";
+export const SUCCESS_700 = "#027A48";
+export const SUCCESS_800 = "#05603A";
+export const SUCCESS_900 = "#054F31";
