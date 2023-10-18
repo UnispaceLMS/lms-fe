0cae5a50029a979eded0bf5ca218bfa4541d711c
Aryaman Rishabh <aryamanrishabh@gmail.com>
2023-10-18 02:45:31 -0400

    add urls

--- src/urls.js
+++ src/urls.js
@@ -1,6 +1,8 @@
 import { apiURL } from "@/constants";
 
 const urls = {
+  login: `${apiURL}/auth/login`,
+  validateToken: `${apiURL}/validate`,
   studentCreateUpdate: `${apiURL}/student/createOrUpdate`,
 };
 
