791f5aac7ad841084db89a47e175ba7bc84b67e5
Aryaman Rishabh <aryamanrishabh@gmail.com>
2023-10-18 02:49:17 -0400

    update redux

--- src/redux/Slices/index.js
+++ src/redux/Slices/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
+import authSlice from "./authSlice";
 import rosterSlice from "./rosterSlice";
 import sidebarSlice from "./sidebarSlice";
 
 const rootReducer = combineReducers({
+  auth: authSlice,
   roster: rosterSlice,
   sidebar: sidebarSlice,
 });
