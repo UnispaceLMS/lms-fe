9c82f48c133aba40941eedc54da7abac9b66157d
Aryaman Rishabh <aryamanrishabh@gmail.com>
2023-10-24 14:50:36 -0400

    update urls

--- src/urls.js
+++ src/urls.js
@@ -1,8 +1,12 @@
 import { apiURL } from "@/constants";
 
 const urls = {
+  // auth
   login: `${apiURL}/auth/login`,
   validateToken: `${apiURL}/auth/validate`,
+
+  //student
+  roster: `${apiURL}/student/roster`,
   studentDetails: `${apiURL}/student/fetch`,
   studentCreateUpdate: `${apiURL}/student/createOrUpdate`,
 };
