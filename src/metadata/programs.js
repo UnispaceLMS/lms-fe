a2a63d062527e84214d0f47f34eb1c4eff2aca4e
Aryaman Rishabh <aryamanrishabh@gmail.com>
2023-11-11 13:52:27 -0500

    add metadata

--- src/metadata/programs.js
+++ src/metadata/programs.js
@@ -1,14 +1,7 @@
 export const studentPrograms = [
-  {
-    value: "program-1",
-    label: "Program 1",
-  },
-  {
-    value: "program-2",
-    label: "Program 2",
-  },
-  {
-    value: "program-3",
-    label: "Program 3",
-  },
+  { value: "INDEPENDENCE_PREP", label: "Independence Prep" },
+  { value: "INDEPENDENCE_COLLEGE", label: "Independence College" },
+  { value: "SKILL_BUILDING", label: "Skill Building" },
+  { value: "INDEPENDENT_LIVING", label: "Independent Living" },
+  { value: "FAMILY_CONSULTING", label: "Family Consulting" },
 ];
---
+++ src/metadata/strengthsConcerns.js
@@ -0,0 +1,9 @@
+export const strengthEnums = {
+  family: "STRENGTH_FAMILY",
+  student: "STRENGTH_STUDENT",
+};
+
+export const concernEnums = {
+  family: "CONCERN_FAMILY",
+  student: "CONCERN_STUDENT",
+};
