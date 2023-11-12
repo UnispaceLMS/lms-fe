6159a3ff876dc7c09a6877f8b30c5b09a8f08a53
Aryaman Rishabh <aryamanrishabh@gmail.com>
2023-11-11 19:04:22 -0500

    update metadata

--- src/metadata/assessments.js
+++ src/metadata/assessments.js
@@ -1,17 +1,17 @@
 export const assessmentOptions = [
-  { value: "member-reporting", label: "Member Reporting" },
-  { value: "observation", label: "Observation" },
-  { value: "rubric", label: "Rubric" },
-  { value: "work-samples", label: "Work samples" },
-  { value: "informal-assessment", label: "Informal Assessment" },
-  { value: "formal-assessment", label: "Formal Assessment" },
+  { value: "MEMBER_REPORTING", label: "Member Reporting" },
+  { value: "OBSERVATION", label: "Observation" },
+  { value: "RUBRIC", label: "Rubric" },
+  { value: "WORK_SAMPLES", label: "Work samples" },
+  { value: "INFORMAL", label: "Informal Assessment" },
+  { value: "FORMAL", label: "Formal Assessment" },
 ];
 
 export const assessmentStyles = {
-  rubric: { color: "#C4320A", bgColor: "#FFF6ED" },
-  observation: { color: "#026AA2", bgColor: "#F0F9FF" },
-  "work-samples": { color: "#C4320A", bgColor: "#FFF6ED" },
-  "member-reporting": { color: "#363F72", bgColor: "#F8F9FC" },
-  "formal-assessment": { color: "#5925DC", bgColor: "#F4F3FF" },
-  "informal-assessment": { color: "#C4320A", bgColor: "#FFF6ED" },
+  RUBRIC: { color: "#C4320A", bgColor: "#FFF6ED" },
+  FORMAL: { color: "#5925DC", bgColor: "#F4F3FF" },
+  INFORMAL: { color: "#C4320A", bgColor: "#FFF6ED" },
+  OBSERVATION: { color: "#026AA2", bgColor: "#F0F9FF" },
+  WORK_SAMPLES: { color: "#C4320A", bgColor: "#FFF6ED" },
+  MEMBER_REPORTING: { color: "#363F72", bgColor: "#F8F9FC" },
 };
--- src/metadata/goals.js
+++ src/metadata/goals.js
@@ -1,9 +1,120 @@
-export const goalOptions = [
-  { value: "cooking-schedule", label: "Cooking Schedule" },
-  { value: "cooking", label: "Cooking" },
-  { value: "grocery", label: "Grocery" },
-  { value: "recipe", label: "Recipe" },
-  { value: "preparing", label: "Preparing" },
-  { value: "meal-portioning", label: "Meal Portioning" },
-  { value: "portion-tracking", label: "Portion Tracking" },
+export const healthAndWellnessGoals = [
+  { value: "MEAL_PLANNING", label: "Meal Planning" },
+  { value: "COOKING_SKILLS", label: "Cooking Skills" },
+  { value: "KITCHEN_SAFETY", label: "Kitchen Safety" },
+  { value: "FOOD_SAFETY", label: "Food Safety" },
+  { value: "MEDICATION_AND_ILLNESS", label: "Medication and Illness" },
+  { value: "HEALTHY_HABITS", label: "Health Habits" },
+  { value: "MENTAL_HEALTH", label: "Mental Health" },
+  { value: "EATING_OUT", label: "Eating Out" },
+];
+
+export const personalManagementGoals = [
+  { value: "PERSONAL_CARE", label: "Personal Care" },
+  { value: "CLOTHING", label: "Clothing" },
+  { value: "PLANNING_AND_SCHEDULING", label: "Planning & Scheduling" },
+  { value: "CALENDARS_AND_CONTACTS", label: "Calendars & Contacts" },
+  { value: "MAILING_AND_POSTAGE", label: "Mailing & Postage" },
+  { value: "SELF_REGULATION", label: "Self Regulation" },
+  { value: "SELF_ADVOCACY", label: "Self Advocacy" },
+  { value: "SELF_AWARENESS", label: "Self Awareness" },
+  { value: "LEISURE_SILLS", label: "Leisure Skills" },
+  { value: "TECH_SKILLS", label: "Tech Skills" },
+  { value: "COMMUNITY_ENGAGEMENT", label: "Community Engagement" },
+];
+
+export const healthyRelationshipGoals = [
+  { value: "CONVERSATION_SKILLS", label: "Conversation Skills" },
+  { value: "LIVING_WITH_OTHERS", label: "Living with others" },
+  { value: "GUESTS_AND_ENTERTAINING", label: "Guests & Entertaining" },
+  {
+    value: "GROUP_INTERACTION_PARTICIPATION",
+    label: "Group Interaction & Participation",
+  },
+  {
+    value: "PERSONAL_INTIMATE_RELATIONSHIPS",
+    label: "Personal/Intimate Relationships",
+  },
+  { value: "RELATIONSHIP_SAFETY", label: "Relationship Safety" },
+  { value: "INTERPERSONAL", label: "Interpersonal" },
+  { value: "SOCIAL_AWARENESS", label: "Social Awareness" },
+];
+
+export const safetyGoals = [
+  { value: "SAFETY_PRACTICES", label: "Safety Practices" },
+  { value: "COMMUNITY_SAFETY", label: "Community Safety" },
+  { value: "HOME_SAFETY", label: "Home Safety" },
+  { value: "FIRST_AID", label: "First Aid" },
+  { value: "PERSONAL_SAFETY", label: "Personal Safety" },
+  { value: "ONLINE_SAFETY", label: "Online Safety" },
+];
+
+export const homeManagementGoals = [
+  { value: "ENTERING_EXITING_HOUSING", label: "Entering / Exiting Housing" },
+  { value: "PROBLEM_SOLVING", label: "Problem Solving" },
+  {
+    value: "GENERAL_CLEANING_HOUSEKEEPING",
+    label: "General Cleaning/Housekeeping",
+  },
+  {
+    value: "BATHROOM_KITCHEN_BEDROOM_CLEANING",
+    label: "Bathroom/Kitchen/Bedroom Cleaning",
+  },
+  { value: "HOME_ORGANIZATION", label: "Home Organization" },
+  { value: "APPLIANCE_USE", label: "Appliance Use" },
+  { value: "GENERAL_MAINTENANCE", label: "General Maintenance" },
+  { value: "HOME_REPAIRS", label: "Home Repairs" },
+];
+
+export const employmentGoals = [
+  {
+    value: "ENTERING_EXITING_EMPLOYMENT",
+    label: "Entering / Exiting Employment",
+  },
+  { value: "APPEARANCE_HYGIENE", label: "Appearance/Hygiene" },
+  {
+    value: "TASK_COMPLETION_JOB_LEARNING",
+    label: "Task Completion/Job Learning",
+  },
+  {
+    value: "ATTENDANCE_TIMESHEET_PAYCHECKS",
+    label: "Attendance, Time Sheets & Paychecks",
+  },
+  { value: "COWORKER_RELATIONSHIPS", label: "Coworker Relationships" },
+  { value: "JOB_SAFETY", label: "Job Safety" },
+  { value: "POSITIVE_WORKER_BEHAVIORS", label: "Positive worker behaviors" },
+];
+
+export const moneyManagementGoals = [
+  {
+    value: "ACCOUNTING_BANKING",
+    label: "Accounting / Banking",
+  },
+  { value: "BUDGETING_BILLS", label: "Budgeting and Bills" },
+  {
+    value: "DECISION_MAKING",
+    label: "Decision Making",
+  },
+  {
+    value: "SHOPPING_DECISIONS_PLANNING",
+    label: "Shopping Decisions & Planning",
+  },
+  { value: "SHOPPING_SKILLS", label: "Shopping Skills" },
+  { value: "CALCULATING", label: "Calculating" },
+];
+
+export const transportationGoals = [
+  {
+    value: "TRAVEL_PLANNING_KNOWLEDGE",
+    label: "Travel Planning & Knowledge",
+  },
+  { value: "TRAVEL_DECISIONS", label: "Travel Decisions" },
+  {
+    value: "LOCATION_CONCEPTS",
+    label: "Location Concepts",
+  },
+  {
+    value: "LEISURE_INFREQUENT_TRAVEL_OPTIONS",
+    label: "Leisure / Infrequent Travel & Travel Options",
+  },
 ];
